9cae0982dbf409a643a5d2dbf39ce424e1a817af
Simon Miezanskas <smiezanskas@ivisgroup.com>
2025-05-10 11:28:34 +0100

    Initial structure created

--- SEOProviderTests/GoogleSEOStatsProviderTests.cs
+++ SEOProviderTests/GoogleSEOStatsProviderTests.cs
@@ -5,7 +5,7 @@ using Xunit;
 public class GoogleSEOStatsProviderTests
 {
     [Fact]
-    public async Task GetSeoStats_ShouldReturnCorrectPositions()
+    public async Task GetSeoStats_ShouldReturnCorrectPositions_WithMoreThanOne_Result()
     {
         // Arrange
         var mockHtmlFetcher = Substitute.For<IHtmlFetcher>();
@@ -27,5 +27,32 @@ public class GoogleSEOStatsProviderTests
         // Assert
         Assert.NotNull(result);
         Assert.Equal(new[] { 1, 3 }, result.Positions);
+    }
+
+    [Fact]
+    public async Task GetSeoStats_ShouldReturnCorrectPositions_FromHtmlFile()
+    {
+        // Arrange
+        var mockHtmlFetcher = Substitute.For<IHtmlFetcher>();
+
+        // Construct the relative path to the HTML file
+        string filePath = Path.Combine(AppContext.BaseDirectory, "MockData", "SampleGoogleQueryResult.html");
+
+        // Read the HTML content from the file
+        string testHtml = await File.ReadAllTextAsync(filePath);
+
+        // Configure the mock to return the file content
+        mockHtmlFetcher.FetchHtmlAsync(Arg.Any<string[]>()).Returns(Task.FromResult(testHtml));
+
+        var provider = new GoogleSEOStatsProvider(mockHtmlFetcher);
+
+        // Act
+        var result = await provider.GetSeoStats(new[] { "land", "registry", "search" }, "infotrack.co.uk");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.NotEmpty(result.Positions);
+        Assert.All(result.Positions, position => Assert.True(position > 0));
+        Assert.Equal(20, result.Positions[0]);
     }
 }
